f1c59252aea5fdd8cdb509233fe8063b4f255a62
Hamzah <hamzah1faisal@gmail.com>
2026-07-31 16:46:20 +0400

    Drawing of basics shapes complete

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ int main()
     const int screenWidth{1280};
     const int screenHeight{800};
 
+    const int ballRadius{20};
+
+    const int rectangleWidth{25};
+    const int rectangleHeight{120};
+
     InitWindow(screenWidth, screenHeight, "My Pong Game!");
 
     SetTargetFPS(60);
@@ -12,6 +17,17 @@ int main()
     while (!(WindowShouldClose()))
     {
         BeginDrawing();
+
+        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
+
+        DrawCircle(screenWidth / 2, screenHeight / 2, ballRadius, WHITE);
+
+        DrawRectangle(10, screenHeight / 2 - rectangleHeight / 2,
+                      rectangleWidth, rectangleHeight, WHITE);
+        DrawRectangle(screenWidth - rectangleWidth - 10,
+                      screenHeight / 2 - rectangleHeight / 2, rectangleWidth,
+                      rectangleHeight, WHITE);
+
         EndDrawing();
     }
 
